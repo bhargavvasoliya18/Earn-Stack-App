e5a59fc3e8d2a55c69f9431aebc91253e2a53904
Ravi Dudhagara <ravidudhagara@Ravis-MacBook-Air.local>
2024-10-05 15:24:46 +0530

    set read and completed quiz

--- lib/src/Screens/MainScreen/HomeScreen/home_screen.dart
+++ lib/src/Screens/MainScreen/HomeScreen/home_screen.dart
@@ -198,7 +198,7 @@ class _HomeScreenProviderState extends State<HomeScreenProvider> with WidgetsBin
                                                             GestureDetector(
                                                               onTap: ()async{state.selectArticleId = item.id.toString(); state.notifyListeners(); await launchURL(item.url ?? "");},
                                                               child: Container(
-                                                                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: AppColors.colorC2C2CC),
+                                                                decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: state.articleList[index].isQuizComplete == true ? Colors.red : AppColors.blue),
                                                                 child: Padding(
                                                                   padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
                                                                   child: Text("Read more", style: TextStyleTheme.customTextStyle(AppColors.white, 14, FontWeight.w400,),),
